e4760fbb00eb525f92bbecbab54ef54ef013b085
Franco Piergallini Guida <francopierguida@gmail.com>
2021-12-13 17:31:13 -0300

    blacklight implementation

--- art/defences/detector/evasion/blacklight/detector.py
+++ art/defences/detector/evasion/blacklight/detector.py
@@ -26,22 +26,39 @@ import numpy as np
 import hashlib
 
 
-
 class Blacklight:
     """
 
     """
     def __init__(self, window_size: int = 20,
                  sliding_step: int = 1,
-                 n_hashes_compare: int = 1000):
+                 n_hashes_match_threshold: int = 20):
+        if window_size < n_hashes_match_threshold:
+            raise ValueError("Number of hashes to match must be less than window size")
+
         self.window_size = window_size
         self.sliding_step = sliding_step
-        self.n_hashes_compare = n_hashes_compare
+        self.n_hashes_match_threshold = n_hashes_match_threshold
+        self.hashes = []
 
     def scan(self, x_adv):
         x_adv = self.process_input(x_adv)
         x_adv_vectors = self.get_x_adv_vectores(x_adv)
-        print(x_adv_vectors)
+        decision, n_intersection = self.compare_overlap(x_adv_vectors)
+        return decision, n_intersection
+
+    def compare_overlap(self, x_adv_vectors):
+        for i in range(len(self.hashes)):
+            try:
+                hashes = self.hashes[i][:self.n_hashes_match_threshold]
+                n_intersection = len(list(set(hashes) & set(x_adv_vectors)))
+            except Exception as e:
+                print(e)
+                pass
+
+        return n_intersection >= self.n_hashes_match_threshold, n_intersection
+
+
 
     def get_x_adv_vectores(self, x_adv):
         N_windows = int((x_adv.shape[0] - self.window_size + self.sliding_step) / self.sliding_step)
@@ -50,9 +67,7 @@ class Blacklight:
             x_adv_slice = x_adv[i:self.window_size]
             x_adv_slice_hash = hashlib.sha3_256(x_adv_slice).hexdigest()
             vectors.append(x_adv_slice_hash)
-
         return sorted(vectors[:self.n_hashes_compare])
-
 
     def process_input(self, x_adv: str) -> np.ndarray:
         if isinstance(x_adv, str):
@@ -61,4 +76,3 @@ class Blacklight:
             x_adv = Image.fromarray(x_adv)
         x_adv = x_adv.quantize(256)
         return x_adv.__array__().flatten('C')
-
